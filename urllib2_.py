c05b308742fdf67078cf6421b7828e67c2ea48f6
errorld <errorld@outlook.com>
2015-09-23 09:35:58 +0800

    use html.encode(utf-8) and shows 中文

--- urllib2_.py
+++ urllib2_.py
@@ -1,6 +1,6 @@
 import urllib.request
 response = urllib.request.urlopen('http://www.baidu.com')
 html = response.read()
-print(html)
+print(html.decode('utf-8'))
 input('response=')
 print(response)
