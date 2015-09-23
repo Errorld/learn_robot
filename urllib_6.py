aac048e15efaeed11f9d3d0a4b55ed5a27e64961
errorld <errorld@outlook.com>
2015-09-23 23:14:42 +0800

    second target done

---
+++ urllib_6.py
@@ -0,0 +1,29 @@
+import urllib.request
+# create a password manager
+password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+
+#伪装浏览器
+target = 'http://192.168.1.1'
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windos NT)'
+headers = {'User-Agent': user_agent}
+req = urllib.request.Request(target, headers=headers)
+
+# Add the username and password.
+username = 'admin'  #家里路由器的默认用户名密码
+password = 'admin'
+# If we knew the realm, we could use it instead of ``None``.
+top_level_url = "http://192.168.1.1"
+password_mgr.add_password(None, top_level_url, username, password)
+
+handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+
+# create "opener" (OpenerDirector instance)
+opener = urllib.request.build_opener(handler)
+
+# use the opener to fetch a URL
+response = opener.open(req)
+print(response.read().decode('gbk'))
+
+# Install the opener.
+# Now all calls to urllib2.urlopen use our opener.
+#urllib2.install_opener(opener)
